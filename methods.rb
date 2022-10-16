2ce7aa954ca66ffe3f5a31c4334591f01ec884ae
ashley <ashley.oketo@student.moringaschool.com>
2022-10-16 18:54:18 +0300

    Completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,40 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+
+def greet(name = "name")
+    puts "Hello, #{name}! "
+end
+
+greet();
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default();
+
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+add(2, 2);
+
+
+def halve(number)
+    if   type of number != "number"
+    return nil
+end 
+
+  return number / 2
+
+end
+
+halve();
